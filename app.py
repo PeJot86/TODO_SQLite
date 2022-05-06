89f38a9b854a76263ee0e00d16d348cd8cb9a1d0
Przemysław Jarocki <przemek.jarocki@gmail.com>
2022-05-06 21:59:44 +0200

    post-fix changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, abort, make_response, request, jsonify
+from flask import Flask, make_response, jsonify
 from models import todos
 
 
@@ -10,56 +10,48 @@ todos.create_table()
 
 @app.route("/api/todos/", methods=["GET"])
 def todos_list():
-    conn = todos.create_connection()
-    return str (todos.show_all(conn, "todos"))
+    return jsonify (todos.show_all("todos"))
 
 
 @app.route("/api/todos/<int:id>", methods=["GET"])
 def get_todo_id(id): 
-    conn = todos.create_connection()
-    return str(todos.show_where(conn, "todos", todos_id = id ))
+    return jsonify (todos.show_where("todos", todos_id = id ))
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(({"error": "Not found", "status_code": 404}), 404)
+    return make_response(({error: "Not found", "status_code": 404}), 404)
 
 
 @app.route("/api/todos/add", methods=["POST"])
 def create_todo():
-    conn = todos.create_connection()
     task = (
-        4,
+        1,
         "Zabierz parasol",
         "Będzie niezła ulewa",
         "started"
     )
-    conn.commit()
-    return str(todos.add_todos(conn, task))
+    return jsonify (todos.add_todos(task))
 
 
 @app.errorhandler(400)
 def bad_request(error):
-    return make_response(jsonify({"error": "Bad request", "status_code": 400}), 400)
+    return make_response(jsonify({error : "Bad request", "status_code": 400}), 400)
 
 
 @app.route("/api/todos/del", methods=["DELETE"])
 def delete_todo():
-    conn = todos.create_connection()
-    return str (todos.delete_all(conn, "todos"))
+    return jsonify (todos.delete_all("todos"))
 
 
 @app.route("/api/todos/del/<int:id>", methods=["DELETE"])
 def delete_todo_id(id): 
-    conn = todos.create_connection()
-    return str(todos.delete_where(conn, "todos", todos_id = id ))
+    return jsonify (todos.delete_where("todos", todos_id = id ))
 
 
 @app.route("/api/todos/update/<int:id>", methods=["PUT"])
 def update_todo_id(id): 
-    conn = todos.create_connection()
-    conn.close()
-    return str (todos.update_todo(conn, "todos", todos_id = id, title ="Kup chleb"))
+    return jsonify (todos.update_todo("todos", todos_id = id, title ="Kup chleb"))
    
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -2,6 +2,10 @@ import sqlite3
 
 class TodosSQLite:
         
+    def __init__(self, db_file):
+        self.db_file = db_file
+    
+    
     def create_connection(self):
         conn = sqlite3.connect('database.db')
         return conn
@@ -9,96 +13,96 @@ class TodosSQLite:
     
     def create_table(self):
         try:
-            conn = self.create_connection()
-            conn.execute('''
-                CREATE TABLE todos (
-                    todos_id INTEGER PRIMARY KEY NOT NULL,
-                    title TEXT NOT NULL,
-                    description TEXT NOT NULL,
-                    status TEXT NOT NULL
-                );
-            ''')
-            conn.commit()
-            print("Todos table created")
+            with self.create_connection() as conn:
+                conn.execute('''
+                    CREATE TABLE todos (
+                        todos_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                        title TEXT NOT NULL,
+                        description TEXT NOT NULL,
+                        status TEXT NOT NULL
+                    );
+                ''')
+                conn.commit()
+                print("Todos table created")
         except:
             print("Todos table already exist")
         finally:
             conn.close()
 
 
-    def show_all(self, conn, table):
-        conn = self.create_connection()
-        cur = conn.cursor()
-        cur.execute(f"SELECT * FROM {table}")
-        rows = cur.fetchall()
-        return rows
+    def show_all(self, table):
+        with self.create_connection() as conn:
+            cur = conn.cursor()
+            cur.execute(f"SELECT * FROM {table}")
+            rows = cur.fetchall()
+            return rows
 
         
-    def show_where(self, conn, table, **query):
-        conn = self.create_connection()
-        cur = conn.cursor()
-        qs = []
-        values = ()
-        for k, v in query.items():
-            qs.append(f"{k}=?")
-            values += (v,)
-        q = " AND ".join(qs)
-        cur.execute(f"SELECT * FROM {table} WHERE {q}", values)
-        rows = cur.fetchall()
-        return rows
+    def show_where(self, table, **query):
+        with self.create_connection() as conn:
+            cur = conn.cursor()
+            qs = []
+            values = ()
+            for k, v in query.items():
+                qs.append(f"{k}=?")
+                values += (v,)
+            q = " AND ".join(qs)
+            cur.execute(f"SELECT * FROM {table} WHERE {q}", values)
+            rows = cur.fetchall()
+            return rows
     
     
-    def add_todos(self, conn, data):
+    def add_todos(self, data):
         sql = '''INSERT OR IGNORE INTO todos(todos_id, title, description, status)
                     VALUES(?,?,?,?)'''
-        conn = self.create_connection()
-        cur = conn.cursor()
-        cur.execute(sql, data)
-        conn.commit()
-        return cur.lastrowid
+        with self.create_connection() as conn:
+            cur = conn.cursor()
+            cur.execute(sql, (data))
+            conn.commit()
+            return cur.lastrowid    
 
 
-    def delete_all(self, conn, table):
-        conn = self.create_connection()
-        sql = f'DELETE FROM {table}'
-        cur = conn.cursor()
-        cur.execute(sql)
-        conn.commit()
-        return print("Deleted")    
+    def delete_all(self, table):
+        with self.create_connection() as conn:
+            sql = f'DELETE FROM {table}'
+            cur = conn.cursor()
+            cur.execute(sql)
+            conn.commit()
+            return print("Deleted")    
 
 
-    def delete_where(self, conn, table, **kwargs):
+    def delete_where(self, table, **kwargs):
             qs = []
             values = tuple()
             for k, v in kwargs.items():
                 qs.append(f"{k}=?")
                 values += (v,)
             q = " AND ".join(qs)
-            conn = self.create_connection()
-            sql = f'DELETE FROM {table} WHERE {q}'
-            cur = conn.cursor()
-            cur.execute(sql, values)
-            conn.commit()
-            print("Deleted")
+            with self.create_connection() as conn:
+                sql = f'DELETE FROM {table} WHERE {q}'
+                cur = conn.cursor()
+                cur.execute(sql, values)
+                conn.commit()
+                print("Deleted")
 
     
-    def update_todo(self, conn, table, todos_id, **kwargs):
-        conn = self.create_connection()
-        parameters = [f"{k} = ?" for k in kwargs]
-        parameters = ", ".join(parameters)
-        values = tuple(v for v in kwargs.values())
-        values += (todos_id, )
+    def update_todo(self, table, todos_id, **kwargs):
+        with self.create_connection() as conn:
+            parameters = [f"{k} = ?" for k in kwargs]
+            parameters = ", ".join(parameters)
+            values = tuple(v for v in kwargs.values())
+            values += (todos_id, )
 
-        sql = f''' UPDATE {table}
-                    SET {parameters}
-                    WHERE todos_id = ?'''
-        try:
-            cur = conn.cursor()
-            cur.execute(sql, values)
-            conn.commit()
-            print("OK")
-        except sqlite3.OperationalError as e:
-            print(e)
+            sql = f''' UPDATE {table}
+                        SET {parameters}
+                        WHERE todos_id = ?'''
+            try:
+                cur = conn.cursor()
+                cur.execute(sql, values)
+                conn.commit()
+                print("OK")
+            except sqlite3.OperationalError as e:
+                print(e)
 
   
-todos = TodosSQLite()
+todos = TodosSQLite("database.db")
